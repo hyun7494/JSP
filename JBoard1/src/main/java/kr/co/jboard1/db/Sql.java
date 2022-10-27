f950ba831059e8440af3614359b4c7971b5bce55
et009153 <et009153@gmail.com>
2022-10-27 13:54:53 +0900

    2022/10/27 13:54 학원수정

--- JBoard1/src/main/java/kr/co/jboard1/db/Sql.java
+++ JBoard1/src/main/java/kr/co/jboard1/db/Sql.java
@@ -22,5 +22,45 @@ public class Sql {
 	public static final String SELECT_TERMS      = "select * from `board_terms`";
 	
 	// board
+	public static final String INSERT_ARTICLE = "insert into `board_article` set "
+												+ "`title`=?,"
+												+ "`content`=?,"
+												+ "`file`=?,"
+												+ "`uid`=?,"
+												+ "`regip`=?,"
+												+ "`rdate`=NOW()";
+	
+	public static final String INSERT_FILE = "insert into `board_file` set "
+											+ "`parent`=?,"
+											+ "`newName`=?,"
+											+ "`oriName`=?,"
+											+ "`rdate`=NOW()";
+	
+	public static final String SELECT_MAX_NO = "select max(`no`) from `board_article`";
+	public static final String SELECT_COUNT_TOTAL = "SELECT COUNT(`no`) FROM `board_article`";	
+	public static final String SELECT_ARTICLES = "SELECT a.*, b.`nick` FROM `board_article` AS a "
+												+ "JOIN `board_user` AS b ON a.uid = b.uid "
+												+ "ORDER BY `no` DESC "
+												+ "LIMIT ?, 10";
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
