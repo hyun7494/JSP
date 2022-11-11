9153c8b48cd416928d7e01ce2f5b33bff739b6f5
et009153 <et009153@gmail.com>
2022-11-11 13:54:30 +0900

    2022/11/11 13:54 학원수정

--- Ch09/src/main/java/controller/user1/RegisterController.java
+++ Ch09/src/main/java/controller/user1/RegisterController.java
@@ -9,7 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import service.user1.RegisterService;
+import dao.User1Dao;
+import vo.User1Vo;
 
 @WebServlet("/user1/register.do")
 public class RegisterController extends HttpServlet {
@@ -20,18 +21,29 @@ public class RegisterController extends HttpServlet {
 	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		requestProc(req, resp);
-	}
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		requestProc(req, resp);
-	}
-	
-	private void requestProc(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		RegisterService service = RegisterService.getInstance();
-		String view = service.requestProc(req, resp);
-		
-		RequestDispatcher dispatcher = req.getRequestDispatcher(view);
+		// View forward
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/user1/register.jsp");
 		dispatcher.forward(req, resp);
 	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// POST 요청
+		String uid  = req.getParameter("uid");
+		String name = req.getParameter("name");
+		String hp   = req.getParameter("hp");
+		String age  = req.getParameter("age");
+		
+		User1Vo vo = new User1Vo();
+		vo.setUid(uid);
+		vo.setName(name);
+		vo.setHp(hp);
+		vo.setAge(age);
+
+		User1Dao.getInstance().insertUser(vo);
+		
+		// 리다이렉트
+		resp.sendRedirect("/Ch09/user1/list.do");
+	}
+	
 }
