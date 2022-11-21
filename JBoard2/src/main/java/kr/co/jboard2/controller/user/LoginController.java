bf7069dc827be11836035ebd836bc0f0ff23418b
et009153 <et009153@gmail.com>
2022-11-21 13:54:39 +0900

    2022/11/21 13:54 학원

---
+++ JBoard2/src/main/java/kr/co/jboard2/controller/user/LoginController.java
@@ -0,0 +1,63 @@
+package kr.co.jboard2.controller.user;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+
+import kr.co.jboard2.dao.UserDAO;
+import kr.co.jboard2.vo.UserVO;
+
+@WebServlet("/user/login.do")
+public class LoginController extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Override
+	public void init() throws ServletException {
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/user/login.jsp");
+		dispatcher.forward(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		// 데이터 수신
+		String uid = req.getParameter("uid");
+		String pass = req.getParameter("pass");
+		
+		// VO 데이터 생성
+		UserVO vo = new UserVO();
+		vo.setUid(uid);
+		vo.setPass(pass);
+		
+		
+		// 데이터베이스 처리
+		int result = UserDAO.getInstance().selectUser(vo);
+		
+		JsonObject json = new JsonObject();
+		json.addProperty("result", result);
+		
+		PrintWriter writer = resp.getWriter();
+		writer.print(json.toString());
+		
+		if(vo != null) {
+			
+		
+		}
+		
+		// 리다이렉트
+		resp.sendRedirect("/JBoard2/user/login.do");
+	}
+
+}
